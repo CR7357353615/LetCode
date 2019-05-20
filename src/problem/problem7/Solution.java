22b4f9a940f5657a021819f61a9cd35b0d25d69b
CR7357353615 <357353615@qq.com>
2019-05-20 09:51:05 +0800

    7 Integer翻转

---
+++ src/problem/problem7/Solution.java
@@ -0,0 +1,42 @@
+package problem.problem7;
+
+/**
+ * 翻转Integer
+ *
+ * Example 1:
+ *
+ * Input: 123
+ * Output: 321
+ * Example 2:
+ *
+ * Input: -123
+ * Output: -321
+ * Example 3:
+ *
+ * Input: 120
+ * Output: 21
+ *
+ * 解题思路： 从数字个位开始进行剥离，并且在累计sum前要预判是否超过Integer范围
+ */
+public class Solution {
+    public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) {
+                return 0;
+            }
+            if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) {
+                return 0;
+            }
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.reverse(1534236469));
+    }
+}
