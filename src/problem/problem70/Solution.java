84d5fa10e71f074d02f208fcbd37bef8c2f72979
CR7357353615 <357353615@qq.com>
2019-05-20 16:53:27 +0800

    70 爬楼梯的方式计算

---
+++ src/problem/problem70/Solution.java
@@ -0,0 +1,43 @@
+package problem.problem70;
+
+/**
+ * 爬楼梯
+ *
+ * Example 1:
+ *
+ * Input: 2
+ * Output: 2
+ * Explanation: There are two ways to climb to the top.
+ * 1. 1 step + 1 step
+ * 2. 2 steps
+ * Example 2:
+ *
+ * Input: 3
+ * Output: 3
+ * Explanation: There are three ways to climb to the top.
+ * 1. 1 step + 1 step + 1 step
+ * 2. 1 step + 2 steps
+ * 3. 2 steps + 1 step
+ *
+ * 解题思路：递归。i表示当前已经走的步数，n是最终要走到的步数
+ */
+public class Solution {
+    public int climbStairs(int n) {
+        return climb_Stairs(0, n);
+    }
+
+    public int climb_Stairs(int i, int n) {
+        if (i > n) {
+            return 0;
+        }
+        if (i == n) {
+            return 1;
+        }
+        return climb_Stairs(i + 1, n) + climb_Stairs(i + 2, n);
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.climbStairs(3));
+    }
+}
