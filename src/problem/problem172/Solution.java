d212f3cb9cdad48663ebad1e4dc2980a3aa0f9c0
CR7357353615 <357353615@qq.com>
2019-05-25 12:07:48 +0800

    172 给定一正整数，求其阶乘结果尾部有多少0 增加解题思路

--- src/problem/problem172/Solution.java
+++ src/problem/problem172/Solution.java
@@ -10,6 +10,8 @@ package problem.problem172;
  * input: 5
  * output: 1
  *
+ * 解题思路：乘数中只有2和5遇到才会出现0，所以要计算所有乘数中有多少2，5对。但是往往2比5多，所以只要计算5的个数就可以了。
+ * 具体解题思路可见：https://github.com/MisterBooo/LeetCodeAnimation/blob/master/notes/LeetCode%E7%AC%AC172%E5%8F%B7%E9%97%AE%E9%A2%98%EF%BC%9A%E9%98%B6%E4%B9%98%E5%90%8E%E7%9A%84%E9%9B%B6.md
  *
  */
 public class Solution {
