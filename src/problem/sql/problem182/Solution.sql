2536b1a2eb24c9d9507b027952d91588d6a7d774
CR7357353615 <357353615@qq.com>
2019-05-25 12:43:21 +0800

    182 找出重复出现的邮箱地址

---
+++ src/problem/sql/problem182/Solution.sql
@@ -0,0 +1,24 @@
+-- 找出重复出现的邮箱地址
+-- 解题思路：使用分组求个数
+
+-- Write a SQL query to find all duplicate emails in a table named Person.
+--
+-- +----+---------+
+-- | Id | Email   |
+-- +----+---------+
+-- | 1  | a@b.com |
+-- | 2  | c@d.com |
+-- | 3  | a@b.com |
+-- +----+---------+
+-- For example, your query should return the following for the above table:
+--
+-- +---------+
+-- | Email   |
+-- +---------+
+-- | a@b.com |
+-- +---------+
+
+select Email
+from Person
+group by Email
+having count(*) > 1;
